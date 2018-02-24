0f1387008a942cb1f187ab262ed66dadf7c12384
root <schwerpunkt@users.noreply.github.com>
2018-02-24 22:26:52 +0100

    Implemented reduce functionality

--- zfsDiffReport.py
+++ zfsDiffReport.py
@@ -5,6 +5,8 @@ import argparse
 import subprocess
 import os
 from pwd import getpwnam
+import hashlib
+from pathlib import Path
 
 DESCRIPTION = "\
 zfsDiffReport.py generates a report text file from the zfs diff of a given \
@@ -20,20 +22,28 @@ And that's how you report a zfs diff.\
 
 def getArgs():
   parser = argparse.ArgumentParser(description=DESCRIPTION,epilog=EPILOG)
-  
-  parser.add_argument("volume",nargs="+",help="observed ZFS volume(s) e.g.: 'ZPOOL/ZFSVOL'")
-  parser.add_argument("-s","--snapshot",default="",dest="snapshot",help="snapshot identifier e.g.: 'zas_w-utc-'")
-  parser.add_argument("-o","--outdir",default=".",help="report file output directory")
-  parser.add_argument("-f","--filename",nargs="?",const=" ",help="optional filename. If set all volume diffs are written to it. If empty reports are written to stdout.")
-  parser.add_argument("--outfilesuffix",default="_zfsDiffReport.txt",help="suffix for report text file. default: '_zfsDiffReport.txt'")
-  parser.add_argument("-p","--permissions",default="",help="permissions for output file e.g.: 'user'")
-  parser.add_argument("-e","--exclude",action="append",help="multiple definitions possible. Diff lines containing an exclude keyword will be omitted. e.g. '.git'")
-  parser.add_argument("-r","--reduce",action="store_true",help="ZFS lists a file that is deleted and (re)created between snapshots with - and +. Omit those lines when the files' checksums match. And modified folder path lines too.")
-  parser.add_argument("--zfsbinary",default="zfs",help="path to zfs binary. default: 'zfs'")
-  parser.add_argument("-v","--verbose",action="store_true",help="")
-  parser.add_argument("-q","--quiet",action="store_true",help="")
-  
+  parser.add_argument("volume",nargs="+",
+    help="observed ZFS volume(s) e.g.: 'ZPOOL/ZFSVOL'")
+  parser.add_argument("-s","--snapshot",default="",dest="snapshot",
+    help="snapshot identifier e.g.: 'zas_w-utc-'")
+  parser.add_argument("-o","--outdir",default=".",
+    help="report file output directory")
+  parser.add_argument("-f","--filename",nargs="?",const=" ",
+    help="optional filename. If set all volume diffs are written to it. If empty reports are written to stdout.")
+  parser.add_argument("--outfilesuffix",default="_zfsDiffReport.txt",
+    help="suffix for report text file. default: '_zfsDiffReport.txt'")
+  parser.add_argument("-p","--permissions",default="",
+    help="permissions for output file e.g.: 'user'")
+  parser.add_argument("-e","--exclude",action="append",
+    help="multiple definitions possible. Diff lines containing an exclude keyword will be omitted. e.g. '.git'")
+  parser.add_argument("-r","--reduce",action="store_true",
+    help="ZFS lists a file that is deleted and (re)created between snapshots with - and +. Omit those lines when the files' checksums match. And modified folder path lines too.")
+  parser.add_argument("--zfsbinary",default="zfs",
+    help="path to zfs binary. default: 'zfs'")
+  parser.add_argument("-v","--verbose",action="store_true")
+  parser.add_argument("-q","--quiet",action="store_true")
   return parser.parse_args()
+
 
 def handleLogging(args):
   if args.verbose:
@@ -48,19 +58,19 @@ def getSnapshots(volume,snapshotidentifier):
   logging.info("Get snapshot list for {}".format(volume))
   process       = subprocess.Popen(["zfs list -t snapshot -o name -s creation -r {}".format(volume)],shell=True,stdout=subprocess.PIPE)
   stdout,stderr = process.communicate()
-  zfssnapshots  = stdout.decode("ascii").splitlines()
+  zfssnapshots  = stdout.decode("utf-8").splitlines()
 
   if snapshotidentifier != "":
-    logging.debug("Filtering snapshots for {}".format(snapshotidentifier))
+    logging.debug("Filter snapshots for {}".format(snapshotidentifier))
     zfssnapshots = list(filter(lambda x:snapshotidentifier in x, zfssnapshots))
   return zfssnapshots[-2],zfssnapshots[-1]
 
 
 def getSortedDiffLines(snapshot1,snapshot2):
-  logging.info("Creating zfs diff of snapshots {} and {}".format(snapshot1,snapshot2))
+  logging.info("Create zfs diff of snapshots {} and {}".format(snapshot1,snapshot2))
   process       = subprocess.Popen(["zfs diff {} {}".format(snapshot1,snapshot2)],shell=True,stdout=subprocess.PIPE)
   stdout,stderr = process.communicate()
-  difflines     = stdout.decode("ascii").splitlines()
+  difflines     = stdout
 
   # sorting difflines by second column
   return sorted(difflines,key=lambda x: x.split()[1])
@@ -68,31 +78,88 @@ def getSortedDiffLines(snapshot1,snapshot2):
 
 def getFilteredDifflines(difflines,excludes):
   if excludes:
-      for exclude in excludes:
-        logging.info("Exclude lines containing '{}'".format(exclude))
+      logging.info("Exclude lines containing '{}'".format(" or ".join(excludes)))
       difflines = list(filter(lambda x:not any(f in x for f in excludes),difflines))
   return difflines
 
-def getReducedDifflines(difflines):
-  # TODO reduce duplicates after checksum comparison
-  # TODO strip ZPOOL/ZFSVOL information if no -f is set
-  return difflines
+
+def getHash(file):
+  # file = file.replace("\\0040"," ")
+  # TODO hots/Br\\0303\\0274tal Lege fails
+  if Path(file).is_dir():
+    return "000"
+  # code from : https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file?answertab=votes#tab-top
+  hash_alg = hashlib.sha1()
+  with open(file,"rb") as f:
+    for chunk in iter(lambda: f.read(4096), b""):
+      hash_alg.update(chunk)
+  return hash_alg.hexdigest()
+
+
+def getReducedDifflines(difflines,stripVolumePath,mountpoint,snapshot1,snapshot2):
+  zfsstructure = "/.zfs/snapshot/"
+  snapshot1 = mountpoint+zfsstructure+snapshot1.split("@")[1]
+  snapshot2 = mountpoint+zfsstructure+snapshot2.split("@")[1]
+  logging.debug("Reduce lines after hashing some files from {} and {}. stripVolumePath {}".format(snapshot1,snapshot2,stripVolumePath))
+
+  reduceddifflines = []
+  mreduced = False
+  for index,line in enumerate(difflines):
+    # TODO write reduced line file maybe
+
+    if mreduced:
+      mreduced = False
+      logging.debug("Reducing line {}".format(line))
+      continue
+
+    if line.startswith("M") and len(line) > 3:
+      mfile = line.split("{}".format(mountpoint))[1]
+      mhash1 = getHash(snapshot1+mfile)
+      mhash2 = getHash(snapshot2+mfile)
+      line = "{} {} {}".format(line,mhash1,mhash2)
+      if mhash1 == mhash2:
+        logging.debug("Reducing line {}".format(line))
+        continue
+
+    elif line.startswith("-") \
+         and index+1 < len(difflines) \
+         and difflines[index+1].startswith("+"):
+      mfile = line.split("{}".format(mountpoint))[1]
+      pfile = difflines[index+1].split("{}".format(mountpoint))[1]
+      if mfile == pfile:
+        mhash = getHash(snapshot1+mfile)
+        phash = getHash(snapshot1+pfile)
+        line  = "{} {}".format(line,mhash)
+        difflines[index+1] = "{} {}".format(difflines[index+1],phash)
+        if mhash == phash:
+          logging.debug("Reducing line {}".format(line))
+          mreduced = True
+          continue
+
+    if stripVolumePath:
+      line = line.replace(mountpoint,"",1)
+
+    line = " ".join(line.split())
+
+    reduceddifflines.append(line)
+
+  return reduceddifflines
+
 
 def writeReport(difflines,outdir,outfile,outfilesuffix,permissions):
   outpath = outdir+"/"+outfile+outfilesuffix
-  logging.info("Writing to {}".format(outpath))
+  logging.info("Write to {}".format(outpath))
   file = open(outpath,"w")
-  for index,line in enumerate(difflines):
-    file.write("{}\n".format(line))
+  file.write("\n".join(difflines))
   file.close()
 
   if permissions != "":
     logging.debug("Setting permissions for user {}".format(permissions))
     os.chown(outpath,getpwnam(permissions).pw_uid,getpwnam(permissions).pw_gid)
 
+
 def main():
   args = getArgs()
-
   handleLogging(args)
 
   logging.debug("TODO am I root (zfs/permissionschange)")
@@ -107,19 +174,21 @@ def main():
 
   collecteddifflines = []
   for volume in volumes:
+    mountpoint = "/{}".format(volume) # TODO get actual mountpoint
     snapshot1,snapshot2 = getSnapshots(volume,args.snapshot)
   
     difflines = getSortedDiffLines(snapshot1,snapshot2)
     difflines = getFilteredDifflines(difflines,args.exclude)
     if args.reduce:
-      difflines = getReducedDifflines(difflines)
+      stripVolumePath = False if args.filename and len(volumes) > 1 else True
+      difflines = getReducedDifflines(difflines,stripVolumePath,mountpoint,snapshot1,snapshot2)
 
     collecteddifflines = collecteddifflines + difflines
   
     if args.filename:
       if volume == volumes[-1]:
         if args.filename == " ": # report to stdout
-          logging.info("Reporting to stdout")
+          logging.info("Report to stdout")
           print("\n".join(collecteddifflines))
         else:
           outfile = args.filename
